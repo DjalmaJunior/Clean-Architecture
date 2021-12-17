bb3c38222e735f419ae8e643fce8bb6cca0c5763
Djalma Junior <djalmajunior03@outlook.com>
2021-12-16 23:10:24 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,6 +8,6 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
 
-    return { ...accountData, id: result.insertedId.toString() }
+    return MongoHelper.map(accountData, result) as AccountModel
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient } from 'mongodb'
+import { Collection, InsertOneResult, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient | null,
@@ -16,5 +16,12 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (
+    data: any,
+    collection: InsertOneResult<Document>
+  ): { id: string, [others: string]: any } => {
+    return { ...data, id: collection.insertedId.toString() }
   }
 }
